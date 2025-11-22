dcb6fd5ba33f37dc3a0f5839040cebfd1e511d94
William-van254 <bk844746@gmail.com>
2025-11-22 07:08:54 +0630

    start.sh

--- start.sh
+++ start.sh
@@ -10,3 +10,10 @@ cd /EvaMaria
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
+
+# This command starts the fake web server in the background (&)
+python3 web_server.py &
+
+# This command starts the main bot process
+python3 bot.py
+
