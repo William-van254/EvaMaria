1ab2f33a5dcc17eb89d8967f772f35d3a7b90273
William-van254 <bk844746@gmail.com>
2025-11-22 10:20:31 +0630

    Script.py

--- Script.py
+++ Script.py
@@ -1,15 +1,16 @@
 class Script(object):
-    # START_TXT ကို Line 2 မှာ စပါမယ်
+    # 1. START_TXT: Welcome Message (မြန်မာလို)
     START_TXT = """မင်္ဂလာပါ {} 👋
+
 **Cine Collect Bot** ကနေ ကြိုဆိုပါတယ်။🫶
 ကြိုက်နှစ်သက်ရာ **Movie/Series** နာမည်ကို ရိုက်ထည့်ပြီး ရှာဖွေနိုင်ပါတယ်။
 
 **💡 ကူညီမှုလိုပါက အောက်ပါ Help ကို နှိပ်ပါ**"""
 
-    # HELP_TXT ကို ဒီအတိုင်း ထားပါ
+    # 2. HELP_TXT: Help Message (မြန်မာလို)
     HELP_TXT = """
     🙋🏻‍♂️ **အကူအညီ လိုအပ်ပါက**
-    
+
 ○  **ဆက်သွယ်ရန် :** @williamvan23 / @williamvan1
     
 ○  **အခြား Commands များ**
@@ -18,107 +19,16 @@ class Script(object):
     /info  - User အချက်အလက်
     /id    - ID စစ်ဆေးခြင်း
 """
-
-    # ABOUT_TXT ကို ဒီအတိုင်း ထားပါ
+    
+    # 3. ABOUT_TXT: About Message (မြန်မာလို)
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
 \n**Cine Collect ၏ ကိုယ်ပိုင် Bot တစ်ခု ဖြစ်ပါသည်။**
-    
+
 ✯ Main Channel : <a href='https://t.me/cinecollect'>Cine Collect Channel</a>"""
-
-    SOURCE_TXT = """<b>NOTE:</b>
-# ... (အောက်က Code တွေ မပြင်ဘဲ ဆက်သွားပါ)
-
-
-    SOURCE_TXT = """<b>NOTE:</b>
-- Eva Maria is a open source project. 
-#- Source - https://github.com/8769ANURAG/EvaMaria  
-
-<b>DEVS:</b>
-- <a href=https://t.me/Am_RoBots>ᴀᴍ_ᴛᴇᴄʜ</a>"""
-    MANUELFILTER_TXT = """Help: <b>Filters</b>
-
-- Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
-
-<b>NOTE:</b>
-1. eva maria should have admin privillage.
-2. only admins can add filters in a chat.
-3. alert buttons have a limit of 64 characters.
-
-<b>Commands and Usage:</b>
-- /filter - <code>add a filter in chat</code>
-- /filters - <code>list all the filters of a chat</code>
-- /del - <code>delete a specific filter in chat</code>
-- /delall - <code>delete the whole filters in a chat (chat owner only)</code>"""
-    BUTTON_TXT = """Help: <b>Buttons</b>
-
-- Eva Maria Supports both url and alert inline buttons.
-
-<b>NOTE:</b>
-1. Telegram will not allows you to send buttons without any content, so content is mandatory.
-2. Eva Maria supports buttons with any telegram media type.
-3. Buttons should be properly parsed as markdown format
-
-<b>URL buttons:</b>
-<code>[Button Text](buttonurl:https://t.me/sources_cods)</code>
-
-<b>Alert buttons:</b>
-<code>[Button Text](buttonalert:This is an alert message)</code>"""
-    AUTOFILTER_TXT = """Help: <b>Auto Filter</b>
-
-<b>NOTE:</b>
-1. Make me the admin of your channel if it\'s private.
-2. make sure that your channel does not contains camrips, porn and fake files.
-3. Forward the last message to me with quotes.
- I'll add all the files in that channel to my db."""
-    CONNECTION_TXT = """Help: <b>Connections</b>
-
-- Used to connect bot to PM for managing filters 
-- it helps to avoid spamming in groups.
-
-<b>NOTE:</b>
-1. Only admins can add a connection.
-2. Send <code>/connect</code> for connecting me to ur PM
-
-<b>Commands and Usage:</b>
-• /connect  - <code>connect a particular chat to your PM</code>
-• /disconnect  - <codecodeconnect from a chat</code>
-• /connections - <code>list all your connections</code>"""
-    EXTRAMOD_TXT = """Help: <b>Extra Modules</b>
-
-<b>NOTE:</b>
-these are the extra features of Eva Maria
-
-<b>Commands and Usage:</b>
-• /id - <code>get id of a specified user.</code>
-• /info  - <code>get information about a user.</code>
-• /imdb  - <code>get the film information from IMDb source.</code>
-• /search  - <code>get the film information from various sources.</code>"""
-    ADMIN_TXT = """Help: <b>Admin mods</b>
-
-<b>NOTE:</b>
-This module only works for my admiadmins>Commands and Usage:</b>
-• /logs - <code>to get the rescent errors</code>
-• /stats - <code>to get status of files in db.</code>
-• /delete - <code>to delete a specific file from db.</code>
-• /users - <code>to get list of my users and ids.</code>
-• /chats - <code>to get list of the my chats and ids </code>
-• /leave  - <code>to leave from a chat.</code>
-• /disable  -  <code>do disable a chat.</code>
-• /ban  - <code>to ban a user.</code>
-• /unban  - <code>to unban a user.</code>
-• /channel - <code>to get list of total connected channels</code>
-• /broadcast - <code>to broadcast a message to all users</code>"""
-    STATUS_TXT = """★ 𝚃𝙾𝚃𝙰𝙻 𝙵𝙸𝙻𝙴𝚂: <code>{}</code>
-★ 𝚃𝙾𝚃𝙰𝙻 𝚄𝚂𝚄𝚂𝙴𝚁𝚂ode>{}</code>
-★ 𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂: <code>{}</code>
-★ 𝚄𝚂𝙴𝙳 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 
-★ 𝙵𝚁𝙴𝙴 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> """
-    LOG_TEXT_G = """#NewGroup
-Group = {}(<code>{}</code>)
-Total Members = <code>{}</code>
-Added By - {}
+    
+    # 4. LOG_TEXT (Log အတွက် လိုအပ်သည်)
+    LOG_TEXT = """#NewUser
+Name : {}
+ID : {}
 """
-    LOG_TEXT_P = """#NewUser
-ID - <code>{}</code>
-Name - {}
-"""
+    # မလိုအပ်သော SOURCE_TXT, BOT_STATS_TXT, BOT_COMMANDS, MANUELFILTER_TXT, BUTTON_TXT များကို ဖယ်ရှားလိုက်ပါပြီ။
