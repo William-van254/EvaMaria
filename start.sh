2b37eebab57e949bf2588c54baa162829a9137fb
William-van254 <bk844746@gmail.com>
2025-11-22 07:27:54 +0630

    start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 
+# 1. New: Install the missing web server library (aiohttp)
+pip install aiohttp
+
+# 2. Original: Install the main bot dependencies
 pip3 install -U -r requirements.txt
+
+# 3. Start the bot (and web server)
 echo "Starting Bot...."
-python3 bot.py
-
-# This command starts the fake web server in the background (&)
 python3 web_server.py &
-
-# This command starts the main bot process
 python3 bot.py
-
