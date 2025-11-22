59722c322fb22c66195a3dfd4b203fad24669e0d
William-van254 <bk844746@gmail.com>
2025-11-22 10:03:58 +0630

    Script.py

--- Script.py
+++ Script.py
@@ -66,7 +66,7 @@ class Script(object):
     AUTOFILTER_TXT = """Help: <b>Auto Filter</b>
 
 <b>NOTE:</b>
-1. Make me the admin of your channel if it's private.
+1. Make me the admin of your channel if it\'s private.
 2. make sure that your channel does not contains camrips, porn and fake files.
 3. Forward the last message to me with quotes.
  I'll add all the files in that channel to my db."""
